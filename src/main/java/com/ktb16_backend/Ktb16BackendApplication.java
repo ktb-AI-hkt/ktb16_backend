a2477ba9cb06dc2389e7f40d776eb92dd858b5e6
kimyuchan <rladbcks103@naver.com>
2025-12-17 14:45:28 +0900

    initial commit

---
+++ src/main/java/com/ktb16_backend/Ktb16BackendApplication.java
@@ -0,0 +1,13 @@
+package com.ktb16_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ktb16BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ktb16BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ktb16_backend/Ktb16BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktb16_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ktb16BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
